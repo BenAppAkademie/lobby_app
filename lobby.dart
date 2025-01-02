fa27b8d51203c7f661175669750a2c80601de738
Ben-Apps <83467150+Ben-Apps@users.noreply.github.com>
2025-01-02 16:54:11 +0100

    first commit

---
+++ lobby.dart
@@ -0,0 +1,55 @@
+void main() {
+
+Player ben = Player("Benjo32", 0, 0, 0.0);
+
+ben.addKill();
+ben.addKill();
+ben.addKill();
+ben.addKill();
+ben.addKill();
+ben.addKill();
+ben.addKill();
+
+ben.calculateKD();
+
+print(ben.kd);
+print(ben.getKD());
+
+}
+
+
+// spielernamen, kills, death, kd
+
+class Player {
+
+String name;
+int kills;
+int death;
+double kd;
+
+Player(this.name, this.kills, this.death, this.kd);
+
+void addKill() {
+kills += 1;
+}
+
+void addDeath() {
+death += 1;
+}
+
+void calculateKD() {
+  if(death == 0){
+    kd = kills.toDouble();
+  }else{
+kd = kills / death;
+  }
+}
+
+double getKD () {
+  return this.kd;
+}
+
+
+
+}
+
